c7a0b9cf847febbcdd2a33b326be1ccc545f7526
Daniel Valadez <valadezd@oregonstate.edu>
2023-02-03 14:14:48 -0800

    Finished Info Section

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,7 +1,30 @@
+import profile from "../assets/profile-picture.jfif";
+import email from "../assets/Mail.png";
 
-/*Top half of figma design: photo, name, butttons, etc etc.*/
-export default function Info(){
-    return(
-        <h1>Info section</h1>
-    )
+export default function Info() {
+    return (
+        <div>
+            <img
+                src={profile}
+                alt="Daniel Valadez, an Oregon State graduate with a degree in Computer Science."
+                className="profile-picture"
+            />
+            <h1 className="fullname">Daniel Valadez</h1>
+            <h4 className="job-title">Frontend Developer</h4>
+            <h5 className="website-link">
+                <a
+                    href="https://github.com/Daniel-Valadez?tab=repositories"
+                    target="blank"
+                >
+                    Take me to Daniel's Github
+                </a>
+            </h5>
+            <button>
+                <a href="mailto:danielvaladez2017@gmail.com">
+                    <img src={email} />
+                    <span>Email</span>
+                </a>
+            </button>
+        </div>
+    );
 }
