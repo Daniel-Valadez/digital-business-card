ae7be7394fa0e1d9e3c210fc0bb3b8f8a8f28d8a
Daniel Valadez <valadezd@oregonstate.edu>
2023-02-03 15:02:23 -0800

    Finished About Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,13 @@
-export default function About(){
-    return(
-        <h1>About me section</h1>
-    )
+export default function About() {
+    return (
+        <div className="about">
+            <h3>About</h3>
+            <p>
+                I recently graduated from Oregon State University with a
+                Bachelor of Science in Computer Science and a minor in Business. 
+                I am currently working on becoming better with React and other popular 
+                frameworks in hopes of landing my first full-time developer job. 
+            </p>
+        </div>
+    );
 }
