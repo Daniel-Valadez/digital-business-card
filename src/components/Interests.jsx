5dde6cf8d10f339059ba7ec5bfc9f1139d906f1c
Daniel Valadez <valadezd@oregonstate.edu>
2023-02-03 15:08:52 -0800

    Finished Interests Section

--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -1,5 +1,13 @@
-export default function Interests(){
-    return(
-        <h1>Interests sections</h1>
-    )
+export default function Interests() {
+    return (
+        <div className="interests">
+            <h3>Interests</h3>
+            <p>
+                I enjoy watching football, baseball, hanging out with my dog,
+                trying new restaurants, learning new things about astronomy, and
+                working out. Also, some of my favorite video games include Pokemon, Minecraft, 
+                Call of Duty, and Madden. 
+            </p>
+        </div>
+    );
 }
