2b8fcf7f178005b1f9e04ba275f67d1943207f4b
Daniel Valadez <valadezd@oregonstate.edu>
2023-02-03 15:33:50 -0800

    Finished Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,45 @@
-export default function Footer(){
-    return(
-        <h1>This is the footer</h1>
-    )
+import facebook from "../assets/Facebook Icon.svg";
+import instagram from "../assets/Instagram Icon.svg";
+import linkedin from "../assets/Linkedin Icon.svg";
+import github from "../assets/GitHub Icon.svg";
+
+export default function Footer() {
+    return (
+        <footer>
+            <a
+                href="https://www.facebook.com/daniel.valadez.73/"
+                target="blank"
+                className="facebook-link"
+            >
+                <img
+                    src={facebook}
+                    alt="An image linking to Daniel's Facebook"
+                />
+            </a>
+            <a href="https://www.instagram.com/daniel_valadez_/" target="blank">
+                <img
+                    src={instagram}
+                    alt="An image linking to Daniel's Instagram."
+                />
+            </a>
+            <a
+                href="https://www.linkedin.com/in/daniel-valadez-9a3556182/"
+                target="blank"
+            >
+                <img
+                    src={linkedin}
+                    alt="An image linking to Daniel's LinkedIn profile."
+                />
+            </a>
+            <a
+                href="https://github.com/Daniel-Valadez?tab=repositories"
+                target="blank"
+            >
+                <img
+                    src={github}
+                    alt="An image linking to Daniel's GitHub account."
+                />
+            </a>
+        </footer>
+    );
 }
